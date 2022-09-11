f04e614fc77242b5b054afd4e61675b0f54398c6
Matt McKenna <matt@mtmckenna.com>
2022-09-11 11:43:03 -0700

    remove debug

--- src/stage.ts
+++ src/stage.ts
@@ -308,18 +308,18 @@ export default class Stage {
   //   );
   // }
 
-  strokeCell(cell, color) {
-    const y = (this.numCellsTall - 1) * this.cellSize;
-    const scale = this.renderer.offset.z;
+  // strokeCell(cell, color) {
+  //   const y = (this.numCellsTall - 1) * this.cellSize;
+  //   const scale = this.renderer.offset.z;
 
-    this.renderer.ctx.strokeStyle = color;
-    this.renderer.ctx.strokeRect(
-      (cell.x * this.cellSize - this.renderer.offset.x) * scale,
-      (y - cell.y * this.cellSize - this.renderer.offset.y) * scale,
-      this.cellSize * scale,
-      this.cellSize * scale
-    );
-  }
+  //   this.renderer.ctx.strokeStyle = color;
+  //   this.renderer.ctx.strokeRect(
+  //     (cell.x * this.cellSize - this.renderer.offset.x) * scale,
+  //     (y - cell.y * this.cellSize - this.renderer.offset.y) * scale,
+  //     this.cellSize * scale,
+  //     this.cellSize * scale
+  //   );
+  // }
 
   neighbors(cell) {
     const result = [];
